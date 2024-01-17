71314823cbb10527f2f7bd00ab8a0aa228c245cf
moontai0724 <moontai0724@gmail.com>
2024-01-17 23:53:48 +0800

    feat(view): multiple single page build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { resolve } from "path";
+import { readdirSync } from "fs";
+
+const alias = readdirSync(resolve(__dirname, "src/views")).reduce(
+  (acc, value) => {
+    acc[value] = resolve(__dirname, `src/views/${value}`);
+    return acc;
+  },
+  {
+    "@": resolve(__dirname, "src"),
+  }
+);
+
+const pages = readdirSync(resolve(__dirname, "src/views/pages")).reduce(
+  (acc, name) => {
+    acc[name] = resolve(__dirname, `src/views/pages/${name}/index.html`);
+    return acc;
+  },
+  {}
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  root: "./src/views",
+  plugins: [vue(), viteSingleFile({ removeViteModuleLoader: true })],
+  publicDir: false,
+  resolve: {
+    alias,
+  },
+  build: {
+    rollupOptions: {
+      input: pages,
+    },
+  },
+});
