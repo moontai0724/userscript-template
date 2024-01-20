c135d7ccef6a519deb68c1b161c99e892687017e
moontai0724 <moontai0724@gmail.com>
2024-01-20 12:05:43 +0800

    feat(script): load view by alias path

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,13 @@
 import html from "rollup-plugin-html";
+import alias from "@rollup/plugin-alias";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
   input: "src/index.ts",
   plugins: [
+    alias({
+      entries: [{ find: "@views", replacement: __dirname }],
+    }),
     typescript({
       tsconfig: "src/tsconfig.json",
     }),
